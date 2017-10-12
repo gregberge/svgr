9f2a8329cbaec5a4447aa4618fbde25533d759f7
Alexey Bondarenko <alexeybondarenko@me.com>
2017-10-12 19:52:03 +0300

    Remove async from the sync tests

--- src/cli/dirCommand.test.js
+++ src/cli/dirCommand.test.js
@@ -1,12 +1,12 @@
 import { rename } from './dirCommand'
 
 describe('rename', () => {
-  it('should transform fileName to the PascalCase', async () => {
+  it('should transform fileName to the PascalCase', () => {
     const result = rename('camel-case.js')
     expect(result).toBe('CamelCase.js')
   })
 
-  it('should change the extension to js', async () => {
+  it('should change the extension to js', () => {
     const result = rename('camel-case.svg')
     expect(result).toBe('CamelCase.js')
   })
