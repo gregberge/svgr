e368f67a45b3a604b9df5cdcf21aff9742557c07
Bogdan Chadkin <trysound@yandex.ru>
2018-09-07 18:40:25 +0300

    Allow to spread props at the start
    The use case is flow which warns about inexact spread. My template looks
    like this and I need to spread props before any other prop.
    
    ```js
    const template = (code, options, state) => `
    // @flow
    // Generated from ${state.filePath}
    
    import * as React from "react";
    
    type Props = {
      size?: string | number,
      fill?: string
    };
    
    const style = {
      display: "block",
      flex: "0 0 auto",
    };
    
    export const ${state.componentName} = ({ size, fill, ...props }: Props) => {
      return (
        ${code}
      );
    };
    `;
    
    ```

--- packages/core/src/h2x/expandProps.test.js
+++ packages/core/src/h2x/expandProps.test.js
@@ -3,12 +3,34 @@ import { transform } from 'h2x-core'
 import expandProps from './expandProps'
 
 describe('expandProps', () => {
-  it('should expand props', () => {
+  it('should expand props in the end by default', () => {
     const result = transform(
-      `<svg>
+      `<svg width="10" height="10">
         <g stroke="#063855" stroke-width="2" />
       </svg>`,
       { plugins: [jsx, expandProps()] },
+    )
+
+    expect(result).toMatchSnapshot()
+  })
+
+  it('should expand props in the end with option', () => {
+    const result = transform(
+      `<svg width="10" height="10">
+        <g stroke="#063855" stroke-width="2" />
+      </svg>`,
+      { plugins: [jsx, expandProps('end')] },
+    )
+
+    expect(result).toMatchSnapshot()
+  })
+
+  it('should expand props at the start with option', () => {
+    const result = transform(
+      `<svg width="10" height="10">
+        <g stroke="#063855" stroke-width="2" />
+      </svg>`,
+      { plugins: [jsx, expandProps('start')] },
     )
 
     expect(result).toMatchSnapshot()
