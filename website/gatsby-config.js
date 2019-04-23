cf9c24f17356f106a86cf2c7cf557d002f258f34
Greg Bergé <greg@smooth-code.com>
2019-04-23 14:04:05 +0200

    docs(website): add ads

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
           { title: 'Playground', url: '/playground/' },
           { title: 'Usage', url: '/docs/' },
         ],
+        codeFundProperty: 269,
       },
     },
   ],
