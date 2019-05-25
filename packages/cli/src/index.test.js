6f8f5fe6880d1b82c00dc4e2b9fbf647d0bd45d1
Bogdan Chadkin <trysound@yandex.ru>
2019-05-25 11:22:59 +0300

    [cli] output relative destination paths
    Currently wide output looks bad on half of the screen I use for terminal
    
    ```
    src/svgs/visibility.svg -> /Users/_________________________________________/src/icons/Visibility.js
    ```
    
    In this diff I suggest simply cut useless path and get this
    
    ```
    src/svgs/visibility.svg -> src/icons/Visibility.js
    ```

--- packages/cli/src/index.test.js
+++ packages/cli/src/index.test.js
@@ -48,8 +48,9 @@ describe('cli', () => {
     expect(result).toMatchSnapshot()
   }, 10000)
 
-  it('should transform a whole directory', async () => {
-    await cli('--out-dir __fixtures_build__/whole __fixtures__')
+  it('should transform a whole directory and output relative destination paths', async () => {
+    const result = await cli('--out-dir __fixtures_build__/whole __fixtures__')
+    expect(result).toMatchSnapshot()
   }, 10000)
 
   it('should support --prettier-config as json', async () => {
