990ae133f43f47d02b4b0eff5918a2d2b1dc99e2
Bogdan Chadkin <trysound@yandex.ru>
2019-05-25 11:41:32 +0300

    Sort output for stable test

--- packages/cli/src/index.test.js
+++ packages/cli/src/index.test.js
@@ -50,7 +50,11 @@ describe('cli', () => {
 
   it('should transform a whole directory and output relative destination paths', async () => {
     const result = await cli('--out-dir __fixtures_build__/whole __fixtures__')
-    expect(result).toMatchSnapshot()
+    const sorted = result
+      .split(/\n/)
+      .sort()
+      .join('\n')
+    expect(sorted).toMatchSnapshot()
   }, 10000)
 
   it('should support --prettier-config as json', async () => {
