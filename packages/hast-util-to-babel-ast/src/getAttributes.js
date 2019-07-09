4669920bc7a4d6c42c179af14e786d47fe12b112
Bogdan Chadkin <trysound@yandex.ru>
2019-07-09 16:10:33 +0300

    Upgrade svg-parser with hast support

---
+++ packages/hast-util-to-babel-ast/src/getAttributes.js
@@ -0,0 +1,70 @@
+import * as t from '@babel/types'
+import { isNumeric, kebabCase, replaceSpaces } from './util'
+import stringToObjectStyle from './stringToObjectStyle'
+import { ATTRIBUTE_MAPPING, ELEMENT_ATTRIBUTE_MAPPING } from './mappings'
+
+function convertAriaAttribute(kebabKey) {
+  const [aria, ...parts] = kebabKey.split('-')
+  return `${aria}-${parts.join('').toLowerCase()}`
+}
+
+function getKey(key, value, node) {
+  const lowerCaseKey = key.toLowerCase()
+  const mappedElementAttribute =
+    ELEMENT_ATTRIBUTE_MAPPING[node.name] &&
+    ELEMENT_ATTRIBUTE_MAPPING[node.name][lowerCaseKey]
+  const mappedAttribute = ATTRIBUTE_MAPPING[lowerCaseKey]
+
+  if (mappedElementAttribute || mappedAttribute) {
+    return t.jsxIdentifier(mappedElementAttribute || mappedAttribute)
+  }
+
+  const kebabKey = kebabCase(key)
+
+  if (kebabKey.startsWith('aria-')) {
+    return t.jsxIdentifier(convertAriaAttribute(kebabKey))
+  }
+
+  if (kebabKey.startsWith('data-')) {
+    return t.jsxIdentifier(kebabKey)
+  }
+
+  return t.jsxIdentifier(key)
+}
+
+function getValue(key, value) {
+  // Handle className
+  if (Array.isArray(value)) {
+    return t.stringLiteral(replaceSpaces(value.join(' ')))
+  }
+
+  if (key === 'style') {
+    return t.jsxExpressionContainer(stringToObjectStyle(value))
+  }
+
+  if (isNumeric(value)) {
+    return t.jsxExpressionContainer(t.numericLiteral(Number(value)))
+  }
+
+  return t.stringLiteral(replaceSpaces(value))
+}
+
+const getAttributes = node => {
+  const keys = Object.keys(node.properties)
+  const attributes = []
+  let index = -1
+
+  while (++index < keys.length) {
+    const key = keys[index]
+    const value = node.properties[key]
+    const attribute = t.jsxAttribute(
+      getKey(key, value, node),
+      getValue(key, value, node),
+    )
+    attributes.push(attribute)
+  }
+
+  return attributes
+}
+
+export default getAttributes
--- packages/hast-util-to-babel-ast/src/index.js
+++ packages/hast-util-to-babel-ast/src/index.js
@@ -1,53 +1,10 @@
-import * as t from '@babel/types'
-import getProps from './getProps'
-import { ELEMENT_TAG_NAME_MAPPING } from './mappings'
+import * as handlers from './handlers'
+import one from './one'
 
-/* Transform the children of `parent`. */
-function all(parent) {
-  const nodes = parent.children || []
-  const { length } = nodes
-  const values = []
-  let index = -1
-
-  while (++index < length) {
-    const result = one(nodes[index], parent)
-    values.push(result)
-  }
-
-  return values.filter(node => node)
-}
-
-function one(node, parent) {
-  if (typeof node === 'string') {
-    return t.jsxExpressionContainer(t.stringLiteral(node))
-  }
-
-  const children = all(node)
-  const selfClosing = children.length === 0
-
-  const name = ELEMENT_TAG_NAME_MAPPING[node.name] || node.name
-
-  const openingElement = t.jsxOpeningElement(
-    t.jsxIdentifier(name),
-    getProps(node.name, node.attributes),
-    selfClosing,
-  )
-
-  const closingElement = !selfClosing
-    ? t.jsxClosingElement(t.jsxIdentifier(name))
-    : null
-
-  const jsxElement = t.jsxElement(openingElement, closingElement, children)
-
-  if (parent == null) {
-    return t.expressionStatement(jsxElement)
-  }
-
-  return jsxElement
-}
+const h = { handlers }
 
 function toBabelAST(tree) {
-  return t.program([one(tree)])
+  return one(h, tree)
 }
 
 export default toBabelAST
