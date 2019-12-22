9d7fb3429277c2cdf042943cda2c188957b4b115
Greg Bergé <greg@smooth-code.com>
2019-12-22 18:54:54 +0100

    feat(cli): generate index.js when --out-dir is used
    Closes #189

--- packages/cli/src/dirCommand.js
+++ packages/cli/src/dirCommand.js
@@ -4,10 +4,10 @@ import { promisify } from 'util'
 import path from 'path'
 import chalk from 'chalk'
 import outputFileSync from 'output-file-sync'
-import readdir from 'recursive-readdir'
 import { convertFile, stat, transformFilename, CASE, politeWrite } from './util'
 
 const access = promisify(fs.access)
+const readdir = promisify(fs.readdir)
 
 async function exists(file) {
   try {
@@ -34,51 +34,61 @@ export function isCompilable(filename) {
   return COMPILABLE_EXTENSIONS.includes(ext)
 }
 
-async function dirCommand(
+export default async function dirCommand(
   program,
   filenames,
   { ext = 'js', filenameCase = CASE.PASCAL, ...options },
 ) {
-  async function write(src, relative) {
-    if (!isCompilable(relative)) return
-    relative = rename(relative, ext, filenameCase)
+  async function write(src, dest) {
+    if (!isCompilable(src)) return null
 
-    const dest = path.resolve(program.outDir, relative)
+    dest = rename(dest, ext, filenameCase)
     const code = await convertFile(src, options)
+    const cwdRelative = path.relative(process.cwd(), dest)
+    const logOutput = `${src} -> ${cwdRelative}\n`
 
     if (program.ignoreExisting && (await exists(dest))) {
-      politeWrite(
-        program,
-        chalk.grey(`${src} -> ${path.relative(process.cwd(), dest)}\n`),
-      )
-      return
+      politeWrite(program, chalk.grey(logOutput))
+      return null
     }
 
     outputFileSync(dest, code)
-    politeWrite(
-      program,
-      chalk.white(`${src} -> ${path.relative(process.cwd(), dest)}\n`),
-    )
+    politeWrite(program, chalk.white(logOutput))
+    return dest
   }
 
-  async function handle(filename) {
+  async function generateIndex(dest, files) {
+    const indexFile = path.join(dest, `index.${ext}`)
+    const exportEntries = files.map(file => {
+      const basename = path.basename(file, path.extname(file))
+      return `export { default as ${basename} } from './${basename}'`
+    })
+    fs.writeFileSync(indexFile, exportEntries.join('\n'))
+  }
+
+  async function handle(filename, root) {
     const stats = await stat(filename)
 
     if (stats.isDirectory(filename)) {
       const dirname = filename
       const files = await readdir(dirname)
-      await Promise.all(
-        files.map(async _filename => {
-          const relative = path.relative(dirname, _filename)
-          return write(_filename, relative)
+      const results = await Promise.all(
+        files.map(async relativeFile => {
+          const absFile = path.join(dirname, relativeFile)
+          return handle(absFile, root)
         }),
       )
-    } else {
-      await write(filename, filename)
+      const transformed = results.filter(Boolean)
+      if (transformed.length) {
+        const dest = path.resolve(program.outDir, path.relative(root, dirname))
+        await generateIndex(dest, transformed)
+      }
+      return null
     }
+
+    const dest = path.resolve(program.outDir, path.relative(root, filename))
+    return write(filename, dest)
   }
 
-  await Promise.all(filenames.map(handle))
+  await Promise.all(filenames.map(file => handle(file, file)))
 }
-
-export default dirCommand
