b5d992d6102362381ac87a91bacc140d231ea230
Greg Bergé <greg@smooth-code.com>
2019-12-24 11:29:43 +0100

    chore(now): fix API

--- api/svgr.js
+++ api/svgr.js
@@ -5,6 +5,10 @@ const { default: svgo } = require('@svgr/plugin-svgo')
 const { default: prettier } = require('@svgr/plugin-prettier')
 
 module.exports = (req, res) => {
+  if (!req.body) {
+    res.status(204).send('')
+    return
+  }
   svgr(req.body.code, { ...req.body.options, plugins: [svgo, jsx, prettier] })
     .then(output => {
       res.status(200).json({ output })
