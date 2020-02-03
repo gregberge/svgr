96966ebaadf78ca5e4fcf9c4263e10406c642a87
Michael Altamirano <michaeljaltamirano@gmail.com>
2020-02-03 15:47:31 -0600

    docs: update CLI options + small website fix (#393)
    * Add --keeping-existing option to CLI
    
    * Update CLI readme with up-to-date options
    
    * Add CLI arg dashes for --title-prop to CLI website docs

--- packages/cli/src/index.js
+++ packages/cli/src/index.js
@@ -49,6 +49,7 @@ program
   )
   .option('-d, --out-dir <dirname>', 'output files into a directory')
   .option('--ignore-existing', 'ignore existing files in output directory')
+  .option('--keep-existing', 'do not override already existing files when used with --out-dir')
   .option('--ext <ext>', 'specify a custom file extension (default: "js")')
   .option(
     '--filename-case <case>',
