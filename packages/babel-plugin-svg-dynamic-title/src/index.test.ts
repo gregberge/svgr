f53e565146e03aaea0c8d8b9770fcfb0d664b4b7
avali4907 <avali4907@outlook.com>
2022-05-26 16:21:06 -0700

    generalized titleplugin

--- packages/babel-plugin-svg-dynamic-title/src/index.test.ts
+++ packages/babel-plugin-svg-dynamic-title/src/index.test.ts
@@ -1,30 +1,40 @@
 import { transform } from '@babel/core'
 import plugin from '.'
 
-const testPlugin = (code: string) => {
+const testTagPlugin = (code: string, tag: string) => {
   const result = transform(code, {
-    plugins: ['@babel/plugin-syntax-jsx', plugin],
+    plugins: ['@babel/plugin-syntax-jsx', () => plugin(tag)],
     configFile: false,
   })
 
   return result?.code
 }
 
-describe('plugin', () => {
+const testTitlePlugin = (code: string) => {
+  return testTagPlugin(code, 'title')
+}
+
+const testDescPlugin = (code: string) => {
+  return testTagPlugin(code, 'desc')
+}
+
+describe('title plugin', () => {
   it('should add title attribute if not present', () => {
-    expect(testPlugin('<svg></svg>')).toMatchInlineSnapshot(
+    expect(testTitlePlugin('<svg></svg>')).toMatchInlineSnapshot(
       `"<svg>{title ? <title id={titleId}>{title}</title> : null}</svg>;"`,
     )
   })
 
   it('should add title element and fallback to existing title', () => {
     // testing when the existing title contains a simple string
-    expect(testPlugin(`<svg><title>Hello</title></svg>`)).toMatchInlineSnapshot(
+    expect(
+      testTitlePlugin(`<svg><title>Hello</title></svg>`),
+    ).toMatchInlineSnapshot(
       `"<svg>{title === undefined ? <title id={titleId}>Hello</title> : title ? <title id={titleId}>{title}</title> : null}</svg>;"`,
     )
     // testing when the existing title contains an JSXExpression
     expect(
-      testPlugin(`<svg><title>{"Hello"}</title></svg>`),
+      testTitlePlugin(`<svg><title>{"Hello"}</title></svg>`),
     ).toMatchInlineSnapshot(
       `"<svg>{title === undefined ? <title id={titleId}>{\\"Hello\\"}</title> : title ? <title id={titleId}>{title}</title> : null}</svg>;"`,
     )
@@ -32,25 +42,72 @@ describe('plugin', () => {
   it('should preserve any existing title attributes', () => {
     // testing when the existing title contains a simple string
     expect(
-      testPlugin(`<svg><title id='a'>Hello</title></svg>`),
+      testTitlePlugin(`<svg><title id='a'>Hello</title></svg>`),
     ).toMatchInlineSnapshot(
       `"<svg>{title === undefined ? <title id={titleId || 'a'}>Hello</title> : title ? <title id={titleId || 'a'}>{title}</title> : null}</svg>;"`,
     )
   })
   it('should support empty title', () => {
-    expect(testPlugin('<svg><title></title></svg>')).toMatchInlineSnapshot(
+    expect(testTitlePlugin('<svg><title></title></svg>')).toMatchInlineSnapshot(
       `"<svg>{title ? <title id={titleId}>{title}</title> : null}</svg>;"`,
     )
   })
   it('should support self closing title', () => {
-    expect(testPlugin('<svg><title /></svg>')).toMatchInlineSnapshot(
+    expect(testTitlePlugin('<svg><title /></svg>')).toMatchInlineSnapshot(
       `"<svg>{title ? <title id={titleId}>{title}</title> : null}</svg>;"`,
     )
   })
 
   it('should work if an attribute is already present', () => {
-    expect(testPlugin('<svg><foo /></svg>')).toMatchInlineSnapshot(
+    expect(testTitlePlugin('<svg><foo /></svg>')).toMatchInlineSnapshot(
       `"<svg>{title ? <title id={titleId}>{title}</title> : null}<foo /></svg>;"`,
+    )
+  })
+})
+
+describe('desc plugin', () => {
+  it('should add desc attribute if not present', () => {
+    expect(testDescPlugin('<svg></svg>')).toMatchInlineSnapshot(
+      `"<svg>{desc ? <desc id={descId}>{desc}</desc> : null}</svg>;"`,
+    )
+  })
+
+  it('should add desc element and fallback to existing desc', () => {
+    // testing when the existing desc contains a simple string
+    expect(
+      testDescPlugin(`<svg><desc>Hello</desc></svg>`),
+    ).toMatchInlineSnapshot(
+      `"<svg>{desc === undefined ? <desc id={descId}>Hello</desc> : desc ? <desc id={descId}>{desc}</desc> : null}</svg>;"`,
+    )
+    // testing when the existing desc contains an JSXExpression
+    expect(
+      testDescPlugin(`<svg><desc>{"Hello"}</desc></svg>`),
+    ).toMatchInlineSnapshot(
+      `"<svg>{desc === undefined ? <desc id={descId}>{\\"Hello\\"}</desc> : desc ? <desc id={descId}>{desc}</desc> : null}</svg>;"`,
+    )
+  })
+  it('should preserve any existing desc attributes', () => {
+    // testing when the existing desc contains a simple string
+    expect(
+      testDescPlugin(`<svg><desc id='a'>Hello</desc></svg>`),
+    ).toMatchInlineSnapshot(
+      `"<svg>{desc === undefined ? <desc id={descId || 'a'}>Hello</desc> : desc ? <desc id={descId || 'a'}>{desc}</desc> : null}</svg>;"`,
+    )
+  })
+  it('should support empty desc', () => {
+    expect(testDescPlugin('<svg><desc></desc></svg>')).toMatchInlineSnapshot(
+      `"<svg>{desc ? <desc id={descId}>{desc}</desc> : null}</svg>;"`,
+    )
+  })
+  it('should support self closing desc', () => {
+    expect(testDescPlugin('<svg><desc /></svg>')).toMatchInlineSnapshot(
+      `"<svg>{desc ? <desc id={descId}>{desc}</desc> : null}</svg>;"`,
+    )
+  })
+
+  it('should work if an attribute is already present', () => {
+    expect(testDescPlugin('<svg><foo /></svg>')).toMatchInlineSnapshot(
+      `"<svg>{desc ? <desc id={descId}>{desc}</desc> : null}<foo /></svg>;"`,
     )
   })
 })
--- packages/babel-plugin-svg-dynamic-title/src/index.ts
+++ packages/babel-plugin-svg-dynamic-title/src/index.ts
@@ -3,25 +3,27 @@ import { NodePath, types as t } from '@babel/core'
 
 const elements = ['svg', 'Svg']
 
-const createTitleElement = (
+const createTagElement = (
+  tag: string,
   children: t.JSXExpressionContainer[] = [],
   attributes: (t.JSXAttribute | t.JSXSpreadAttribute)[] = [],
 ) => {
-  const title = t.jsxIdentifier('title')
+  const eleName = t.jsxIdentifier(tag)
   return t.jsxElement(
-    t.jsxOpeningElement(title, attributes),
-    t.jsxClosingElement(title),
+    t.jsxOpeningElement(eleName, attributes),
+    t.jsxClosingElement(eleName),
     children,
   )
 }
 
-const createTitleIdAttribute = () =>
+const createTagIdAttribute = (tag: string) =>
   t.jsxAttribute(
     t.jsxIdentifier('id'),
-    t.jsxExpressionContainer(t.identifier('titleId')),
+    t.jsxExpressionContainer(t.identifier(`${tag}Id`)),
   )
 
-const addTitleIdAttribute = (
+const addTagIdAttribute = (
+  tag: string,
   attributes: (t.JSXAttribute | t.JSXSpreadAttribute)[],
 ) => {
   const existingId = attributes.find(
@@ -29,28 +31,23 @@ const addTitleIdAttribute = (
   ) as t.JSXAttribute | undefined
 
   if (!existingId) {
-    return [...attributes, createTitleIdAttribute()]
+    return [...attributes, createTagIdAttribute(tag)]
   }
   existingId.value = t.jsxExpressionContainer(
     t.isStringLiteral(existingId.value)
-      ? t.logicalExpression('||', t.identifier('titleId'), existingId.value)
-      : t.identifier('titleId'),
+      ? t.logicalExpression('||', t.identifier(`${tag}Id`), existingId.value)
+      : t.identifier(`${tag}Id`),
   )
   return attributes
 }
 
-// Aveline-art note: this is the plugin that goes into babel; it has a specific format, and is an object with visitor key.
-// Babel first converts the data into an AST tree. Then the plugins are applied to the tree in order to change the tree. Finaly the AST is changed back into its original data form.
-// Plugins uses visitors, which searches for a type of tree node. If the right one is found, it is transformed by the plugin.
-const plugin = () => ({
+const plugin = (tag: string) => ({
   visitor: {
-    // Aveline-art: the identifier, which takes a node
     JSXElement(path: NodePath<t.JSXElement>) {
       if (!elements.length) return
 
       const openingElement = path.get('openingElement')
       const openingElementName = openingElement.get('name')
-      // Aveline-art: check that the parent element is an svg
       if (
         !elements.some((element) =>
           openingElementName.isJSXIdentifier({ name: element }),
@@ -59,23 +56,24 @@ const plugin = () => ({
         return
       }
 
-      // Aveline-art: a helper function, not used yet
-      const getTitleElement = (
+      const getTagElement = (
         existingTitle?: t.JSXElement,
       ): t.JSXExpressionContainer => {
-        const titleExpression = t.identifier('title')
+        const tagExpression = t.identifier(tag)
         if (existingTitle) {
-          existingTitle.openingElement.attributes = addTitleIdAttribute(
+          existingTitle.openingElement.attributes = addTagIdAttribute(
+            tag,
             existingTitle.openingElement.attributes,
           )
         }
         const conditionalTitle = t.conditionalExpression(
-          titleExpression,
-          createTitleElement(
-            [t.jsxExpressionContainer(titleExpression)],
+          tagExpression,
+          createTagElement(
+            tag,
+            [t.jsxExpressionContainer(tagExpression)],
             existingTitle
               ? existingTitle.openingElement.attributes
-              : [createTitleIdAttribute()],
+              : [createTagIdAttribute(tag)],
           ),
           t.nullLiteral(),
         )
@@ -86,7 +84,7 @@ const plugin = () => ({
             t.conditionalExpression(
               t.binaryExpression(
                 '===',
-                titleExpression,
+                tagExpression,
                 t.identifier('undefined'),
               ),
               existingTitle,
@@ -97,28 +95,26 @@ const plugin = () => ({
         return t.jsxExpressionContainer(conditionalTitle)
       }
 
-      // store the title element,  Aveline-art is null for now
-      let titleElement: t.JSXExpressionContainer | null = null
+      // store the title element
+      let tagElement: t.JSXExpressionContainer | null = null
 
-      // aveline-art check whether a title is in the svgs children
       const hasTitle = path.get('children').some((childPath) => {
-        if (childPath.node === titleElement) return false
+        if (childPath.node === tagElement) return false
         if (!childPath.isJSXElement()) return false
         const name = childPath.get('openingElement').get('name')
         if (!name.isJSXIdentifier()) return false
-        if (name.node.name !== 'title') return false
-        // aveline-art verified that there is a title element, now run the childpath through the helper function, then replace the current AST node with new childpath
-        titleElement = getTitleElement(childPath.node)
-        childPath.replaceWith(titleElement)
+        if (name.node.name !== tag) return false
+        tagElement = getTagElement(childPath.node)
+        childPath.replaceWith(tagElement)
         return true
       })
 
       // create a title element if not already create
-      titleElement = titleElement || getTitleElement()
+      tagElement = tagElement || getTagElement()
       if (!hasTitle) {
         // path.unshiftContainer is not working well :(
         // path.unshiftContainer('children', titleElement)
-        path.node.children.unshift(titleElement)
+        path.node.children.unshift(tagElement)
         path.replaceWith(path.node)
       }
     },
